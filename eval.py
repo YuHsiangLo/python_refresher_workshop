6e2b58ed53a1899df231f056dab2966df09643d7
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2022-06-21 09:16:17 -0700

    Added last q

--- eval.py
+++ eval.py
@@ -121,14 +121,22 @@ def cv_syll(ipas):
     for index, symbol in enumerate(ipas):
         current_syll.append(symbol)
         if symbol in VOWEL:
+            # the end of the current syllable, append it!
             sylls.append((current_syll, start, index + 1))
 
             start = index + 1
             current_syll = []
 
-        if index == len(ipas) - 1:
+        if index == len(ipas) - 1:  # end of ipa list
+
+            # handle cases like [z] or [d]
             if sylls == []:
                 sylls.append(([], start, index + 1))
+
+            # ə ɹ ɛ s t
+            # sylls = [[ə], [ɹ, ɛ]]
+            # current_syll = [[s, t]]
+            # take out the last syllable [ɹ, ɛ], combine it with current_syll
 
             sylls[-1] = (sylls[-1][0] + current_syll,
                          sylls[-1][1],
@@ -168,7 +176,7 @@ def clever_syll(ipas):
                 current_syll = current_syll[1:]
                 start += 1
 
-            seen_stop = not seen_stop
+        seen_stop = not seen_stop
 
         if symbol in VOWEL:
             sylls.append((current_syll, start, index + 1))
@@ -199,6 +207,314 @@ with open('./data/dev.tsv', mode='r') as f:
 
 evaluate(dev_syll, baseline_syll)
 
-print(cv_syll(['ə', 'd', 'æ', 'p', 't', 'ə']))
-print(clever_syll(['ə', 'd', 'æ', 'p', 's', 't', 'ə']))
-print(clever_syll(['z']))
+with open('./data/dev.tsv', mode='r') as f:
+    for line in f:
+        _, _, syll = line.strip().split(sep='\t')
+
+        delimited = []
+        for i, symbol in enumerate(syll[:-1]):
+            if symbol != '.' and syll[i+1] != '.':
+                delimited.extend([symbol, '.'])
+            else:
+                delimited.append(symbol)
+        delimited.append(syll[-1])
+
+        print(delimited)
+
+from collections import Counter
+from pprint import pprint
+from math import log
+
+bound_counter_1 = Counter()
+no_bound_counter_1 = Counter()
+
+bound_counter_2 = Counter()
+no_bound_counter_2 = Counter()
+
+cv_bound_counter_1 = Counter()
+cv_no_bound_counter_1 = Counter()
+
+cv_bound_counter_2 = Counter()
+cv_no_bound_counter_2 = Counter()
+
+NUM_IPA = len(IPAS)
+
+bound_count = 0
+no_bound_count = 0
+
+
+def determine_cv(ipa):
+    if ipa in CONS:
+        return 'C'
+    elif ipa in VOWEL:
+        return 'V'
+    else:
+        return '#'
+
+
+with open('./data/train.tsv', mode='r') as f:
+    for line in f:
+        _, _, syll = line.strip().split(sep='\t')
+        syll = syll.split(sep=' ')
+
+        delimited = []
+        for i, symbol in enumerate(syll[:-1]):
+            if symbol != '.' and syll[i + 1] != '.':
+                delimited.extend([symbol, '|'])
+            else:
+                delimited.append(symbol)
+
+        delimited.append(syll[-1])
+        delimited = ['#', '|'] + delimited + ['|', '#']
+
+        for i in range(2, len(delimited) - 2):
+            if delimited[i] == '|':
+                no_bound_count += 1
+
+                cv_no_bound_counter_2[
+                    (
+                    determine_cv(delimited[i - 3]), determine_cv(delimited[i - 1]), '_', determine_cv(delimited[i + 1]),
+                    determine_cv(delimited[i + 3]))
+                ] += 1
+                cv_no_bound_counter_1[
+                    (determine_cv(delimited[i - 1]), '_', determine_cv(delimited[i + 1]))
+                ] += 1
+
+                no_bound_counter_2[
+                    (delimited[i - 3], delimited[i - 1], '_', delimited[i + 1], delimited[i + 3])
+                ] += 1
+                no_bound_counter_1[
+                    (delimited[i - 1], '_', delimited[i + 1])
+                ] += 1
+            elif delimited[i] == '.':
+                bound_count += 1
+
+                cv_bound_counter_2[
+                    (
+                    determine_cv(delimited[i - 3]), determine_cv(delimited[i - 1]), '_', determine_cv(delimited[i + 1]),
+                    determine_cv(delimited[i + 3]))
+                ] += 1
+                cv_bound_counter_1[
+                    (determine_cv(delimited[i - 1]), '_', determine_cv(delimited[i + 1]))
+                ] += 1
+
+                bound_counter_2[
+                    (delimited[i - 3], delimited[i - 1], '_', delimited[i + 1], delimited[i + 3])
+                ] += 1
+                bound_counter_1[
+                    (delimited[i - 1], '_', delimited[i + 1])
+                ] += 1
+
+pprint(cv_no_bound_counter_2)
+
+with open('./data/dev.tsv', mode='r') as f:
+    for line in f:
+        _, _, syll = line.strip().split(sep='\t')
+        syll = syll.split(sep=' ')
+
+        delimited = []
+        for i, symbol in enumerate(syll[:-1]):
+            if symbol != '.' and syll[i + 1] != '.':
+                delimited.extend([symbol, '|'])
+            else:
+                delimited.append(symbol)
+
+        delimited.append(syll[-1])
+        delimited = ['#', '|'] + delimited + ['|', '#']
+
+        for i in range(2, len(delimited) - 2):
+            if delimited[i] == '|':
+                no_bound_count += 1
+
+                cv_no_bound_counter_2[
+                    (
+                    determine_cv(delimited[i - 3]), determine_cv(delimited[i - 1]), '_', determine_cv(delimited[i + 1]),
+                    determine_cv(delimited[i + 3]))
+                ] += 1
+                cv_no_bound_counter_1[
+                    (determine_cv(delimited[i - 1]), '_', determine_cv(delimited[i + 1]))
+                ] += 1
+
+                no_bound_counter_2[
+                    (delimited[i - 3], delimited[i - 1], '_', delimited[i + 1], delimited[i + 3])
+                ] += 1
+                no_bound_counter_1[
+                    (delimited[i - 1], '_', delimited[i + 1])
+                ] += 1
+            elif delimited[i] == '.':
+                bound_count += 1
+
+                cv_bound_counter_2[
+                    (
+                    determine_cv(delimited[i - 3]), determine_cv(delimited[i - 1]), '_', determine_cv(delimited[i + 1]),
+                    determine_cv(delimited[i + 3]))
+                ] += 1
+                cv_bound_counter_1[
+                    (determine_cv(delimited[i - 1]), '_', determine_cv(delimited[i + 1]))
+                ] += 1
+
+                bound_counter_2[
+                    (delimited[i - 3], delimited[i - 1], '_', delimited[i + 1], delimited[i + 3])
+                ] += 1
+                bound_counter_1[
+                    (delimited[i - 1], '_', delimited[i + 1])
+                ] += 1
+
+# pprint(bound_counter_1)
+
+demon_b_counter_2 = sum(bound_counter_2.values()) + (NUM_IPA + 1) ** 4
+demon_b_counter_1 = sum(bound_counter_1.values()) + (NUM_IPA + 1) ** 2
+
+demon_n_counter_2 = sum(no_bound_counter_2.values()) + (NUM_IPA + 1) ** 4
+demon_n_counter_1 = sum(no_bound_counter_1.values()) + (NUM_IPA + 1) ** 2
+
+demon_cv_b_counter_2 = sum(cv_bound_counter_2.values()) + 3 ** 4
+demon_cv_b_counter_1 = sum(cv_bound_counter_1.values()) + 3 ** 2
+
+demon_cv_n_counter_2 = sum(cv_no_bound_counter_2.values()) + 3 ** 4
+demon_cv_n_counter_1 = sum(cv_no_bound_counter_1.values()) + 3 ** 2
+
+bound_counter_2 = {k: log((v + 1) / demon_b_counter_2) for k, v in bound_counter_2.items()}
+bound_counter_1 = {k: log((v + 1) / demon_b_counter_1) for k, v in bound_counter_1.items()}
+
+no_bound_counter_2 = {k: log((v + 1) / demon_n_counter_2) for k, v in no_bound_counter_2.items()}
+no_bound_counter_1 = {k: log((v + 1) / demon_n_counter_1) for k, v in no_bound_counter_1.items()}
+
+cv_bound_counter_2 = {k: log((v + 1) / demon_cv_b_counter_2) for k, v in cv_bound_counter_2.items()}
+cv_bound_counter_1 = {k: log((v + 1) / demon_cv_b_counter_1) for k, v in cv_bound_counter_1.items()}
+
+cv_no_bound_counter_2 = {k: log((v + 1) / demon_cv_n_counter_2) for k, v in cv_no_bound_counter_2.items()}
+cv_no_bound_counter_1 = {k: log((v + 1) / demon_cv_n_counter_1) for k, v in cv_no_bound_counter_1.items()}
+
+pprint(bound_counter_2)
+pprint(cv_no_bound_counter_1)
+
+import re
+
+b_prior = log(bound_count / (bound_count + no_bound_count))
+n_prior = log(no_bound_count / (bound_count + no_bound_count))
+
+test_gold = [d['syll'] for d in test]
+test_sys = []
+
+with open('./data/test.tsv', mode='r') as f:
+    for line in f:
+        _, ipa, corr = line.strip().split(sep='\t')
+        ipa = ['#', ' '] + re.split(r'( )', ipa) + [' ', '#']
+        corr = corr.split(sep=' ')
+
+        parsed = []
+
+        for i in range(2, len(ipa) - 2):
+            if ipa[i] == ' ':
+
+                b = bound_counter_2.get((ipa[i - 3], ipa[i - 1], '_', ipa[i + 1], ipa[i + 3]),
+                                        log(1 / demon_b_counter_2)) + \
+                    bound_counter_1.get((ipa[i - 1], '_', ipa[i + 1]), log(1 / demon_b_counter_1)) + \
+                    b_prior
+                n = no_bound_counter_2.get((ipa[i - 3], ipa[i - 1], '_', ipa[i + 1], ipa[i + 3]),
+                                           log(1 / demon_n_counter_2)) + \
+                    no_bound_counter_1.get((ipa[i - 1], '_', ipa[i + 1]), log(1 / demon_n_counter_1)) + \
+                    n_prior
+
+                # b = bound_counter_2.get((ipa[i-3], ipa[i-1], '_', ipa[i+1], ipa[i+3]), log(1/demon_b_counter_2)) +\
+                #    bound_counter_1.get((ipa[i-1], '_', ipa[i+1]), log(1/demon_b_counter_1)) +\
+                #    cv_bound_counter_2.get((determine_cv(ipa[i-3]), determine_cv(ipa[i-1]), '_', determine_cv(ipa[i+1]), determine_cv(ipa[i+3])), log(1/demon_cv_b_counter_2)) +\
+                #    cv_bound_counter_1.get((determine_cv(ipa[i-1]), '_', determine_cv(ipa[i+1])), log(1/demon_cv_b_counter_1)) +\
+                #    b_prior
+                # n = no_bound_counter_2.get((ipa[i-3], ipa[i-1], '_', ipa[i+1], ipa[i+3]), log(1/demon_n_counter_2)) +\
+                #    no_bound_counter_1.get((ipa[i-1], '_', ipa[i+1]), log(1/demon_n_counter_1)) +\
+                #    cv_no_bound_counter_2.get((determine_cv(ipa[i-3]), determine_cv(ipa[i-1]), '_', determine_cv(ipa[i+1]), determine_cv(ipa[i+3])), log(1/demon_cv_n_counter_2)) +\
+                #    cv_no_bound_counter_1.get((determine_cv(ipa[i-1]), '_', determine_cv(ipa[i+1])), log(1/demon_cv_n_counter_1)) +\
+                #    n_prior
+
+                if b > n:
+                    parsed.append('.')
+
+                # print((ipa[i-3], ipa[i-1], '_', ipa[i+1], ipa[i+3]))
+                # print('b:',
+                #      bound_counter_2.get((ipa[i-3], ipa[i-1], '_', ipa[i+1], ipa[i+3]), log(1/demon_b_counter_2)) +
+                #      bound_counter_1.get((ipa[i-1], '_', ipa[i+1]), log(1/demon_b_counter_1)) +
+                #      b_prior)
+                # print('n:',
+                #      no_bound_counter_2.get((ipa[i-3], ipa[i-1], '_', ipa[i+1], ipa[i+3]), log(1/demon_n_counter_2)) +
+                #      no_bound_counter_1.get((ipa[i-1], '_', ipa[i+1]), log(1/demon_n_counter_1)) +
+                #      n_prior)
+            else:
+                parsed.append(ipa[i])
+
+        if parsed != corr:
+            print('parsed:', ''.join(parsed))
+            print('corr:', ''.join(corr))
+            print('----------')
+
+        test_sys.append(get_syll_indices(parsed))
+
+evaluate(test_gold, test_sys)
+
+from collections import Counter
+
+uni_bndry_cntr = Counter()
+uni_non_bndry_cntr = Counter()
+bi_bndry_cntr = Counter()
+bi_non_bndry_cntr = Counter()
+
+with open('./data/train.tsv', mode='r') as f:
+    for line in f:
+        _, _, syll = line.strip().split(sep='\t')
+        syll = syll.split(sep=' ')
+
+        delimited = []
+        for i, symbol in enumerate(syll[:-1]):
+            if symbol != '.' and syll[i + 1] != '.':
+                delimited.extend([symbol, '|'])
+            else:
+                delimited.append(symbol)
+
+        delimited.append(syll[-1])
+        delimited = ['#', '|'] + delimited + ['|', '#']
+
+        for i in range(2, len(delimited) - 2):
+
+            if delimited[i] == '|':
+                uni_non_bndry_cntr[
+                    (delimited[i - 1], '_', delimited[i + 1])
+                ] += 1
+                bi_non_bndry_cntr[
+                    (delimited[i - 3], delimited[i - 1], '_', delimited[i + 1], delimited[i + 3])
+                ] += 1
+            elif delimited[i] == '.':
+                uni_bndry_cntr[
+                    (delimited[i - 1], '_', delimited[i + 1])
+                ] += 1
+                bi_bndry_cntr[
+                    (delimited[i - 3], delimited[i - 1], '_', delimited[i + 1], delimited[i + 3])
+                ] += 1
+
+
+def determine_cv(ipa):
+    if ipa in CONS:
+        return 'C'
+    elif ipa in VOWEL:
+        return 'V'
+    else:
+        return '#'
+
+
+uni_bndry_cv_cntr = Counter()
+uni_non_bndry_cv_cntr = Counter()
+bi_bndry_cv_cntr = Counter()
+bi_non_bndry_cv_cntr = Counter()
+
+for (pre, sep, post), freq in uni_bndry_cntr.items():
+    uni_bndry_cv_cntr[(determine_cv(pre), sep, determine_cv(post))] += freq
+
+for (pre, sep, post), freq in uni_non_bndry_cntr.items():
+    uni_non_bndry_cv_cntr[(determine_cv(pre), sep, determine_cv(post))] += freq
+
+for (prepre, pre, sep, post, postpost), freq in bi_bndry_cntr.items():
+    bi_bndry_cv_cntr[(determine_cv(prepre), determine_cv(pre), sep, determine_cv(post), determine_cv(postpost))] += freq
+
+for (prepre, pre, sep, post, postpost), freq in bi_non_bndry_cntr.items():
+    bi_non_bndry_cv_cntr[
+        (determine_cv(prepre), determine_cv(pre), sep, determine_cv(post), determine_cv(postpost))] += freq
